a25c965bead739e2cabb6dbf257804a07df54850
DoggieBalloon <armaggeddonchief@gmail.com>
2019-05-12 18:03:22 +0800

    Updated car script
    Now it moves really fast but the decceleration doesn't work

--- Scenes/car/car.gd
+++ Scenes/car/car.gd
@@ -1,39 +1,32 @@
 extends KinematicBody2D
 
-var direction : Vector2 = Vector2()
-var speed : float = 0
-var acceleration : float = 1800
-var decceleration = 3000
-
 var motion : Vector2 = Vector2()
-var target_motion : Vector2 = Vector2()
-var steering : Vector2 = Vector2()
+var direction : Vector2 = Vector2()
+
+var speed = 0
+const MAX_SPEED = 600
 const MASS = 2
 
-const MAX_SPEED = 600
-
-var Skin = null
-var target_angle = 0
+export var acceleration = 1800
+export var decceleration = 3000
 
 func _ready():
-	Skin = get_node("AnimatedSprite")
 	set_physics_process(true)
-
+	
 func _physics_process(delta):
 	
 	direction = Vector2()
 	
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("w_key"):
 		direction.y = -1
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("s_key"):
 		direction.y = 1
 	
-	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("a_key"):
 		direction.x = -1
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("d_key"):
 		direction.x = 1
-	
+		
 	if direction != Vector2():
 		speed += acceleration * delta
 	else:
@@ -41,19 +34,8 @@ func _physics_process(delta):
 	
 	speed = clamp(speed, 0, MAX_SPEED)
 	
-	target_motion = speed * direction.normalized() * delta
-	steering = target_motion - motion
-	
-	if steering.length() > 1:
-		steering = steering.normalized()
-	
-	motion += steering / MASS
-	
-	if speed == 0:
-		motion = Vector2()
-	
-	if motion != Vector2():
-		target_angle = atan2(motion.x, motion.y) - PI/2
-		Skin.set_rotation(target_angle)
+	motion = speed * direction.normalized()
 	
 	move_and_slide(motion)
+	
+	print(str(motion))
