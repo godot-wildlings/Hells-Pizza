c932cb724de520a0fd2fa59b13bb4d1f8b859809
plexsoup <plexsoup@gmail.com>
2019-05-12 16:31:49 -0700

    hide warnings

--- Scenes/car/DetectTile.gd
+++ Scenes/car/DetectTile.gd
@@ -8,11 +8,11 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#warning-ignore:unused_argument
 func _process(delta):
 	var my_pos : Vector2 = get_global_position()
 	var cell_location : Vector2 = Game.map.world_to_map(my_pos)
-	var cell = Game.map.get_cell(cell_location.x, cell_location.y)
+	var cell = Game.map.get_cell(int(cell_location.x), int(cell_location.y))
 
 	if cell == Game.map.HOT_LAVA or cell == Game.map.COLD_LAVA:
 		print("LAVA!")
