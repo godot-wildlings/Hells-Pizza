2f51d5a17a737d73dd7f09ceff1ae522a3e115d4
DoggieBalloon <armaggeddonchief@gmail.com>
2019-05-14 16:22:44 +0800

    Work-In-Progress destination code
    WIP destination code. Commented out the code so that it won't crash the game.

--- Scenes/Maps/Underworld/Underworld.gd
+++ Scenes/Maps/Underworld/Underworld.gd
@@ -6,7 +6,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+#	var destination_scene = load("res://Scenes/Maps/DeliveryDestination/DeliveryDestination.tscn")
+#	var new_destination = destination_scene.instance()
+#	var new_position = Vector2(rand_range(Game.player.position, Vector2(0,0)), rand_range(Game.player.position, Vector2(0,0)))
+#	add_child(new_destination)
+#	new_destination.set_global_position(new_position)
+#
+#	Game.player.current_destionation = new_destination
+#
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
