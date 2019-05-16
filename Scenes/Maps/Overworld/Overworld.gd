c362d2e03b41b94d34444dcbb8306bef578ee818
Kevin von Spiczak <caevv@me.com>
2019-05-16 20:53:45 +0200

    rename Devil.tscn to UnderworldPortal.tscn

--- Scenes/Maps/Overworld/Overworld.gd
+++ Scenes/Maps/Overworld/Overworld.gd
@@ -9,26 +9,18 @@ onready var pizza_factory = $PizzaFactory
 func _init():
 	Game.map = self
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	spawn_devil()
+	spawn_portal_to_underworld()
 
-
-func spawn_devil():
-
-	var devil_scene = load("res://Scenes/NPCs/Devil.tscn")
-	var new_devil = devil_scene.instance()
-	$NPCs.add_child(new_devil)
-	new_devil.start()
+func spawn_portal_to_underworld():
+	var portal_scene = load("res://Scenes/Maps/UnderworldPortal/UnderworldPortal.tscn")
+	var new_portal = portal_scene.instance()
+	$NPCs.add_child(new_portal)
+	new_portal.start()
 
 func add_car(car_node):
 	$Cars.add_child(car_node)
 
 func add_pedestrian(pedestrian_node):
 	$Pedestrians.add_child(pedestrian_node)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
--- Scenes/Maps/UnderworldPortal/UnderworldPortal.gd
+++ Scenes/Maps/UnderworldPortal/UnderworldPortal.gd
@@ -1,10 +1,5 @@
 extends Area2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Game.devil = self
 
@@ -22,8 +17,3 @@ func find_safe_location():
 		set_global_position(Vector2.RIGHT.rotated(rand_direction) * distance_from_origin)
 		if $DetectTile.is_safe():
 			safe = true
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
