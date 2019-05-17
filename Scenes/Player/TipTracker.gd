ff646a725e76e2ecff4185242be56eb5482fded6
plexsoup <plexsoup@gmail.com>
2019-05-17 15:21:02 -0700

    Added "return to pizzeria" instructions

--- Scenes/Player/TipTracker.gd
+++ Scenes/Player/TipTracker.gd
@@ -1,8 +1,13 @@
-extends HBoxContainer
+extends Container
 
 var ticks : int = 0
 
 var time_remaining : float = 60.0
+
+onready var cash_on_hand = $TipTracker/CashOnHand
+onready var instructions_label = $InstructionsLabel
+onready var pizza_ammo = $TipTracker/PizzaAmmo
+onready var stop_watch = $TipTracker/StopWatch
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,16 +20,20 @@ func _process(delta):
 
 	if ticks % 20 == 0:
 		var cash_string = "$" + "%6.2f" % Game.player.cash
-		$CashOnHand.set_text(cash_string)
+		cash_on_hand.set_text(cash_string)
 
 	if Game.player.current_destination != Game.map.pizza_factory:
 		time_remaining -= delta
 		update()
 
-	$PizzaAmmo.set_value(Game.player.pizza_ammo)
+	pizza_ammo.set_value(Game.player.pizza_ammo)
+	if Game.player.pizza_ammo == 0:
+		instructions_label.show()
+	else:
+		instructions_label.hide()
 
 func _draw():
-	var clock_rect = $StopWatch
+	var clock_rect = stop_watch
 	var clock_size : Vector2 = clock_rect.get_size()
 	var clock_pos = clock_rect.get_position() + clock_size/2
 	var radius = clock_size.x / 2
